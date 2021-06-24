f0ce652eb89b3d28409bf7d999c3af9269ace865
kumutha <kulasekaram.k@mookambikainfo.com>
2021-06-24 11:36:07 +0530

    folder structure updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Layout from './layout';
+import Layout from './components/layout';
 
 import 'react-bootstrap'
 function App() {
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
-import './App.css'
+
 class Header extends React.Component {
     render() {
         return (
