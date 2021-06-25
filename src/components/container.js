3c534a80f0d291a8eba2c8055235f7cecb2863ef
kumutha <kulasekaram.k@mookambikainfo.com>
2021-06-25 13:28:10 +0530

    delete action updated

--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 class Container extends React.Component {
+    handleRemoveRow = (sno) => {
+        console.log("id", sno)
+        let rows = [this.props.value]
+        rows.splice(sno, 1)
+
+        this.setState({
+            rows: rows
+        })
+    };
     render() {
         return (
             <Table striped bordered hover className="w-50">
@@ -11,12 +20,13 @@ class Container extends React.Component {
                         <th>City</th>
                     </tr>
                 </thead>
-                {this.props.value.map((data, mykey) =>
-                    <tbody key={mykey} >
+                {this.props.value.map((data, index) =>
+                    <tbody key={index} >
                         <tr>
                             <td>{data.sno}</td>
                             <td>{data.name}</td>
                             <td>{data.city}</td>
+                            <td><button onClick={() => this.handleRemoveRow(data.sno)}>Delete</button></td>
                         </tr>
                     </tbody>
                 )}
